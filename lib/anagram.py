5d525b2e054ae0084ee7e68057b0aae334851644
Suleiman <real1sule@gmail.com>
2025-11-25 14:36:03 +0300

    completed anagram-detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word):
+         
+            #sorts letters of the original word
+        sorted_word = sorted(self.word)
+            #compares each word in the list
+        return [w for w in word if sorted(w) == sorted_word]
+
+listen = Anagram('listen')
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
+    
